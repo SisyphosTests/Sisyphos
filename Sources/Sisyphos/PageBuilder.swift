1171b50a437b0c0150a0e3a3c3953b46ec7b19fd
Lukas Stührk <Lukas@Stuehrk.net>
2022-11-13 23:38:30 +0100

    Add the possibility to use conditionals in page declarations.

--- Sources/Sisyphos/PageBuilder.swift
+++ Sources/Sisyphos/PageBuilder.swift
@@ -1,6 +1,37 @@
 
 @resultBuilder public struct PageBuilder {
-   public static func buildBlock(_ components: PageElement...) -> PageDescription {
-       PageDescription(elements: components)
-   }
+    public static func buildBlock(_ components: PageDescriptionBlock...) -> PageDescription {
+        PageDescription(elements: components.flatMap { $0.buildingBlocks })
+    }
+
+    // MARK: - Conditions
+
+    public static func buildIf(_ value: PageDescription?) -> PageDescription {
+        guard let value else { return PageDescription(elements: []) }
+
+        return value
+    }
+
+    public static func buildEither(first component: PageDescription) -> PageDescription {
+        component
+    }
+
+    public static func buildEither(second component: PageDescription) -> PageDescription {
+        component
+    }
+}
+
+
+public protocol PageDescriptionBlock {
+    var buildingBlocks: [PageElement] { get }
+}
+
+
+public extension PageElement {
+    var buildingBlocks: [PageElement] { [self] }
+}
+
+
+extension PageDescription: PageDescriptionBlock {
+    public var buildingBlocks: [PageElement] { elements }
 }

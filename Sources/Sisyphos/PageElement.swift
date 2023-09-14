0efaa61d84b65867a2127f09f512fc80183a1aaa
Ada Chmielewska <adriana.chmielewska@westwing.pl>
2023-09-14 09:43:28 +0200

    Added support for toggle component which is named switch in UITests

--- Sources/Sisyphos/PageElement.swift
+++ Sources/Sisyphos/PageElement.swift
@@ -72,6 +72,36 @@ public struct Button: PageElement {
 }
 
 
+public struct Switch: PageElement {
+    public let elementIdentifier: PageElementIdentifier
+
+    let identifier: String?
+    let label: String?
+
+    public var queryIdentifier: QueryIdentifier {
+        .init(
+            elementType: .switch,
+            identifier: identifier,
+            label: label,
+            value: nil,
+            descendants: []
+        )
+    }
+
+    public init(
+        identifier: String? = nil,
+        label: String? = nil,
+        file: String = #file,
+        line: UInt = #line,
+        column: UInt = #column
+    ) {
+        self.elementIdentifier = .init(file: file, line: line, column: column)
+        self.identifier = identifier
+        self.label = label
+    }
+}
+
+
 public struct NavigationBar: PageElement, HasChildren {
     public let elementIdentifier: PageElementIdentifier
 

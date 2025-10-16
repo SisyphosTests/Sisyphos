2487e540ebd98868933863eab8c62c394f0e151e
Lukas Stührk <Lukas@Stuehrk.net>
2025-10-16 21:04:59 +0200

    Fix unit tests.
    When we introduced the changes to the predicate logic in
    aa8691a0b96f8f3182ebe7aad7219f01a3a298fe, we didn't adjust the
    tests.

--- Tests/SisyphosTests/Internal/NSPredicateTests.swift
+++ Tests/SisyphosTests/Internal/NSPredicateTests.swift
@@ -6,12 +6,7 @@ final class NSPredicateTests: XCTestCase {
     func testPredicateDescription() {
         let predicate = NSPredicate(format: "%@ > 42")
         let sisyphosPredicate = NSPredicate(
-            step: .init(
-                elementType: .alert,
-                identifier: "expected identifier",
-                label: "expected label",
-                value: "expected value"
-            )
+            snapshot: FakeSnapshot()
         )
 
         XCTAssertTrue(predicate.description.starts(with: "<NSComparisonPredicate:"))
@@ -20,4 +15,24 @@ final class NSPredicateTests: XCTestCase {
             sisyphosPredicate.description
         )
     }
+}
+
+
+/// We can't get an instance of an actual XCUIElementSnapshot because we are running unit tests and not UI tests.
+/// But this fake is good enough for testing our NSPredicate logic.
+private class FakeSnapshot: XCUIElementSnapshot {
+    var children: [any XCUIElementSnapshot] = []
+    var dictionaryRepresentation: [XCUIElement.AttributeName : Any] = [:]
+    var identifier: String = "expected identifier"
+    var frame: CGRect = .zero
+    var value: Any? = "expected value"
+    var title: String = "title"
+    var label: String = "expected label"
+    var elementType: XCUIElement.ElementType = .alert
+    var isEnabled: Bool = false
+    var horizontalSizeClass: XCUIElement.SizeClass = .compact
+    var verticalSizeClass: XCUIElement.SizeClass = .compact
+    var placeholderValue: String? = nil
+    var isSelected: Bool = false
+    var hasFocus: Bool = false
 }
